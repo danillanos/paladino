3c989718770a116dbf9d85eda8b961f39529687d
Daniel Llanos <hdaniel.llanos@gmail.com>
2025-09-04 10:40:11 +0000

    for deploy changes

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "paladino",
+      cwd: "/var/www/paladino",
+      script: "node",
+      args: "./node_modules/next/dist/bin/next start -p 3001 -H 127.0.0.1",
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+}
